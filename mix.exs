76a2dde468f0eb88897219c834c613eeae64c7e4
Ruben Amortegui <ruben.amortegui@gmail.com>
2019-11-22 08:40:07 -0700

    Run formatter

--- mix.exs
+++ mix.exs
@@ -57,7 +57,6 @@ defmodule GithubActions.MixProject do
     [
       "ecto.setup": ["ecto.create", "ecto.migrate", "run priv/repo/seeds.exs"],
       "ecto.reset": ["ecto.drop", "ecto.setup"],
-
       test: ["ecto.create --quiet", "ecto.migrate", "test"]
     ]
   end
