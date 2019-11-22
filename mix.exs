7c1ad9cc76d5bc6eeb1d82a356f90549bc8a39de
Ruben Amortegui <ruben.amortegui@gmail.com>
2019-11-21 20:02:27 -0700

    Push

--- mix.exs
+++ mix.exs
@@ -38,6 +38,7 @@ defmodule GithubActions.MixProject do
       {:phoenix, "~> 1.4.11"},
       {:phoenix_pubsub, "~> 1.1"},
       {:phoenix_ecto, "~> 4.0"},
+
       {:ecto_sql, "~> 3.1"},
       {:postgrex, ">= 0.0.0"},
       {:phoenix_html, "~> 2.11"},
