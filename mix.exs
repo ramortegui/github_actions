3140ee7ae4c4f13420eec0a06627b2b5f8330b9a
Ruben Amortegui <ruben.amortegui@gmail.com>
2019-11-21 20:07:14 -0700

    Run formatter.

--- mix.exs
+++ mix.exs
@@ -34,11 +34,9 @@ defmodule GithubActions.MixProject do
   defp deps do
     [
       {:credo, "~> 1.1.0", only: [:dev, :test], runtime: false},
-
       {:phoenix, "~> 1.4.11"},
       {:phoenix_pubsub, "~> 1.1"},
       {:phoenix_ecto, "~> 4.0"},
-
       {:ecto_sql, "~> 3.1"},
       {:postgrex, ">= 0.0.0"},
       {:phoenix_html, "~> 2.11"},
