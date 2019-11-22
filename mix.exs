b0deda6e56f448040404813f23e64aa2f731b169
Ruben Amortegui <ruben.amortegui@gmail.com>
2019-11-21 21:34:24 -0700

    Break master

--- mix.exs
+++ mix.exs
@@ -57,6 +57,7 @@ defmodule GithubActions.MixProject do
     [
       "ecto.setup": ["ecto.create", "ecto.migrate", "run priv/repo/seeds.exs"],
       "ecto.reset": ["ecto.drop", "ecto.setup"],
+
       test: ["ecto.create --quiet", "ecto.migrate", "test"]
     ]
   end
