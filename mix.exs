1a862c119619884a9435d3e490e484b9da242fdf
Ruben Amortegui <ruben.amortegui@gmail.com>
2019-11-21 20:00:50 -0700

    Break mix exs.

--- mix.exs
+++ mix.exs
@@ -34,6 +34,7 @@ defmodule GithubActions.MixProject do
   defp deps do
     [
       {:credo, "~> 1.1.0", only: [:dev, :test], runtime: false},
+
       {:phoenix, "~> 1.4.11"},
       {:phoenix_pubsub, "~> 1.1"},
       {:phoenix_ecto, "~> 4.0"},
